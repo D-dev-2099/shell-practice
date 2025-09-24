8f8e96c1526f2e659e4eb0a92821440a27a63409
D-dev-2099 <d.kommareddy2099@gmail.com>
2025-09-24 15:46:11 +0530

    message

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#no space before or after equal
-PERSON1=Hema
-PERSON2=Ramu
-
-echo "$PERSON1:: Hi $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1,I am fine. How are you?"
-echo "$PERSON1:: what are you learning recently?"
-echo "$PERSON2:: I am learning shell scripting $PERSON1"
+echo "Nani:: Hi chinni, How are you?"
+echo "Chinni:: Hello Nani,I am fine. How are you?"
+echo "Nani:: what are you learning recently?"
+echo "Chinni:: I am learning shell scripting Nani"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#no space before or after equal
+PERSON1=Hema
+PERSON2=Ramu
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1,I am fine. How are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
