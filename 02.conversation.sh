f57157213a9998cc32dc2e61bc97170329b06c04
D-dev-2099 <d.kommareddy2099@gmail.com>
2025-09-24 15:20:11 +0530

    2 file conversation commited

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Nani   :: Hi Chinni, How are you?"
+echo "chinni :: Hello Nani,I am fine. How are you?"
+echo "Nani   :: what are you learning recently?"
+echo "chinni :: I am learning shell scripting Nani"
