a683b9fd31d1fbaa834a641149a92798db9cbf98
D-dev-2099 <d.kommareddy2099@gmail.com>
2025-09-24 15:29:13 +0530

    message

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Nani   :: Hi Chinni, How are you?"
-echo "chinni :: Hello Nani,I am fine. How are you?"
-echo "Nani   :: what are you learning recently?"
-echo "chinni :: I am learning shell scripting Nani"
+#no space before or after equal
+PERSON1=Hema
+PERSON2=Ramu
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1,I am fine. How are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
