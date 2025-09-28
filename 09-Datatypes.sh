620434206e3c2b714e9f437b6846c06b17c681a2
D-dev-2099 <d.kommareddy2099@gmail.com>
2025-09-29 03:20:34 +0530

    Special variables

--- 09-Datatypes.sh
+++ 09-Datatypes.sh
@@ -3,7 +3,15 @@
 # everything in shell is considered as string 
 NUMBER1=100
 NUMBER2=200
+NAME=Devops
 
-SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "SUM is: ${SUM}"
+
+#Size = 4, Max index = 3
+LEADERS=("Modi" "putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}"
