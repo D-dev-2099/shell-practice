d00265c3ce204c91fec795cc2067d9c4ce5e9720
D-dev-2099 <d.kommareddy2099@gmail.com>
2025-09-29 04:37:11 +0530

    conditions

---
+++ 12-installations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo "ERROR:: Please run this script with root privelege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+   echo "ERROR:: installing MYSQL is  SUCCESS"
+fi
