89078eb8d4b7e0ec91f1e74cebaa3bc6834a1a36
D-dev-2099 <d.kommareddy2099@gmail.com>
2025-09-29 06:48:59 +0530

    conditions

---
+++ 13-installations.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: Please run this script with root privelege"
+   exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ #functions receive inputs through args just like shell script args 
+   if [ $1 -ne 0 ]; then
+       echo "ERROR:: installing $2 is  SUCCESS"
+       exit 1 # failure is other than 0
+    else 
+       echo "Installing $2 is SUCCESS"   
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
